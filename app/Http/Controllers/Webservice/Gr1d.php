ea3751375b90db9960bc05bb5be77d128d58f6d3
monymachado <monymachad@gmail.com>
2020-09-07 22:23:03 +0000

    Add Controller Gr1d

--- app/Http/Controllers/Webservice/Gr1d.php
+++ app/Http/Controllers/Webservice/Gr1d.php
@@ -11,46 +11,52 @@ use App\Model\Pessoa;
 class Gr1d extends Controller
 {
     //
-    const ENDPOINT = "https://gateway.gr1d.io/sandbox/dataseek/datasearch/v1/pessoas/?cpf=05588411551";
+    const ENDPOINT = "https://gateway.gr1d.io/sandbox/dataseek/datasearch/v1/pessoas/?cpf=%s";
     /**
      * Acessando a API
      * Armazenando os dados no banco de dados
      * Retorna o objeto
      */
     
-public function getCPF($nome)
+public function searchCPF($cpf)
     {
-
-        $nascimento = nascimento::where('nome', $nome)->first();
-        
-        if(!$nascimento){
+    $pessoa = Pessoa::where('cpf', $cpf)->first();
+    
+        if(!$pessoa){
             // url 
-            $url = sprintf(self::ENDPOINT, $nome);
+            $url = sprintf(self::ENDPOINT, $cpf);
 
+        //curl
+        
+            $request = Http::withHeaders(['X-api-key' => 'token']) -> get($url);
+            $response = $request->json();
+            
+            // "pf": {     "cpf": "11111111111",     "nome": "nome da pessoa fisica",
 
-            //curl
-            $response = Curl::to($url)
-               ->asJson()
-                ->get();
 
        // dados
-        $cpf = $response->cpf;
-        $nome = $response->nome;
-        $nascimento = $response->nascimento;
-        $sexo = $response->sexo;
-        $classe = $response->classe;
-
+        $cpf = $response['pf']['cpf'];  
+        
+        $nome = $response['pf']['nome'];;
+        $nascimento = $response['pf']['dn'];;
+        $sexo = $response['pf']['sexo'];
+        $classe = $response['pf']['classe'];
+        $email = $response['pf']['email'];
+        
+    
        // model
-            $nascimento = nascimento::create([
+            $pessoa = Pessoa::create([
                'cpf' => $cpf,
-               'nome' => $response->nome,
+               'nome' => $nome,
+               'nacimento' => $nascimento,
                'sexo' => $sexo,
                'classe' => $classe,
+               'email' => $email
             ]);
             
         }
         
-        return $nascimento;
+        return $pessoa;
     }
 }
 
